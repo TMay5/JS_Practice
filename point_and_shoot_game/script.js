a819b3cacedf883f396c0dd7494dc3741a11c9b0
TMay5 <127858053+TMay5@users.noreply.github.com>
2023-05-04 11:16:23 +0200

    Update script.js
    updated:
    -new enemy type added
    -background added
    -recolored first enemy sprite sheet for better visibility

--- point_and_shoot_game/script.js
+++ point_and_shoot_game/script.js
@@ -15,6 +15,10 @@ let timeToNextRaven = 0;
 let ravenInterval = 500;
 let lastTime = 0;
 
+let timeToNextSaw = 0;
+let sawInterval = 650;
+let lastSaw = 0;
+
 let ravens = [];
 class Raven {
     constructor(){
@@ -29,7 +33,7 @@ class Raven {
         this.directionY = Math.random() * 5 - 2.5;
         this.markedForDeletion = false;
         this.image = new Image();
-        this.image.src = 'raven.png';
+        this.image.src = 'raven1.png';
         this.frame = 0;
         this.maxFrame = 4;
         this.timeSinceFlap = 0;
@@ -57,7 +61,60 @@ class Raven {
             }
         }
         if (this.x < 0 - this.width) gameOver = true;
-        
+        }
+        draw(){
+            collisionCtx.fillStyle = this.color;
+            collisionCtx.fillRect(this.x, this.y, this.width, this.height);
+            ctx.drawImage(this.image, this.frame * this.spriteWidth, 0, this.spriteWidth, this.spriteHeight, this.x, this.y, this.width, this.height);
+        }
+    }
+
+
+const raven = new Raven();
+
+let saws = [];
+
+class flyingSaw {
+    constructor(){
+        this.spriteWidth = 213;
+        this.spriteHeight = 213;
+        this.sizeModifier = Math.random() * 0.7 + 0.5;
+        this.width = this.spriteWidth * this.sizeModifier;
+        this.height = this.spriteHeight * this.sizeModifier;
+        this.x = canvas.width;
+        this.y = Math.random() * (canvas.height - this.height);
+        this.directionX = Math.random() * 4 + 2;
+        this.directionY = Math.random() * 4 - 2.5;
+        this.markedForDeletion = false;
+        this.image = new Image();
+        this.image.src = 'flyingsaw.png';
+        this.frame = 0;
+        this.maxFrame = 7;
+        this.timeSinceSpin = 0;
+        this.spinInterval = Math.random() * 50 + 80;
+        this.randomColors = [Math.floor(Math.random() * 255), Math.floor(Math.random() * 255), Math.floor(Math.random() * 255)];
+        this.color = 'rgb(' + this.randomColors[0] + ',' + this.randomColors[1] + ',' + this.randomColors[2] + ')';
+        this.hasTrail = Math.random() > 0.2; //returns boolean
+    }
+    update(deltaTime){
+        if (this.y < 0 || this.y > canvas.height - this.height){
+            this.directionY = this.directionY * -1;
+        }
+        this.x -= this.directionX;
+        this.y += this.directionY;
+        if (this.x < 0 - this.width) this.markedForDeletion = true;
+        this.timeSinceSpin += deltaTime;
+        if (this.timeSinceSpin > this.spinInterval){
+            if (this.frame > this.maxFrame) this.frame = 0;
+            else this.frame++;
+            this.timeSinceSpin = 0;
+            if (this.hasTrail){
+                for (let i = 0; i < 7; i++){
+                particles.push(new Particle(this.x, this.y, this.width, this.color));
+                }
+            }
+        }
+        if (this.x < 0 - this.width) gameOver = true;    
     }
     draw(){
         collisionCtx.fillStyle = this.color;
@@ -65,7 +122,7 @@ class Raven {
         ctx.drawImage(this.image, this.frame * this.spriteWidth, 0, this.spriteWidth, this.spriteHeight, this.x, this.y, this.width, this.height);
     }
 }
-const raven = new Raven();
+const saw = new flyingSaw();
 
 let explosions = [];
 
@@ -133,7 +190,7 @@ class Particle {
 function drawScore(){
     ctx.fillStyle = 'black';
     ctx.fillText('Score: ' + score, 50, 75);
-    ctx.fillStyle = 'white';
+    ctx.fillStyle = 'grey';
     ctx.fillText('Score: ' + score, 53, 78);
 }
 
@@ -142,21 +199,20 @@ function drawGameOver(){
     ctx.fillStyle = 'lightblue';
     ctx.fillText('GAME OVER! YOUR SCORE IS: ' + score, canvas.width/2, canvas.height/2);
     ctx.fillStyle = 'black';
-    ctx.fillText('GAME OVER! YOUR SCORE IS: ' + score, canvas.width/2 - 3, canvas.height/2 -3);
+    ctx.fillText('GAME OVER! YOUR SCORE IS: ' + score, canvas.width/2 - 2, canvas.height/2 - 2);
 }
 
 window.addEventListener('click', function(e){
     const detectPixelColor = collisionCtx.getImageData(e.x, e.y, 1, 1);
-    console.log(detectPixelColor);
     const pc = detectPixelColor.data;
-    ravens.forEach(object => {
+    [...ravens, ...saws].forEach(object => {
         if (object.randomColors[0] === pc[0] && object.randomColors[1] === pc[1] && object.randomColors[2] === pc[2]){
             //collision detected
             object.markedForDeletion = true;
             score++;
             explosions.push(new Explosions(object.x, object.y, object.width));
         }
-    });
+    })    
 });
 
 function animate(timestamp){
@@ -165,17 +221,26 @@ function animate(timestamp){
     let deltaTime = timestamp - lastTime;
     lastTime = timestamp;
     timeToNextRaven += deltaTime;
+    timeToNextSaw += deltaTime * 0.3;
     if (timeToNextRaven > ravenInterval){
         ravens.push(new Raven());
         timeToNextRaven = 0;
         ravens.sort(function(a,b){
             return a.width - b.width;
         });
-    };
+    }
+    if (timeToNextSaw > sawInterval){
+        saws.push(new flyingSaw());
+        timeToNextSaw = 0;
+        saws.sort(function(a,b){
+            return a.width - b.width;
+        });
+    }
     drawScore();
-    [...particles, ...ravens, ...explosions].forEach(object => object.update(deltaTime)); //array literal - spread operator
-    [...particles, ...ravens, ...explosions].forEach(object => object.draw());
+    [...particles, ...ravens, ...saws, ...explosions].forEach(object => object.update(deltaTime)); //array literal - spread operator
+    [...particles, ...ravens, ...saws, ...explosions].forEach(object => object.draw());
     ravens = ravens.filter(object => !object.markedForDeletion);
+    saws = saws.filter(object => !object.markedForDeletion);
     explosions = explosions.filter(object => !object.markedForDeletion);
     particles = particles.filter(object => !object.markedForDeletion);
     if (!gameOver) requestAnimationFrame(animate);
